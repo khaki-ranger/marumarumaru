989accf6c2c49088d52c5fadb9e95d9a49d791b6
Yohei Terashima <yamanogomora@gmail.com>
2018-10-31 11:22:42 +0000

    新規ユーザーの情報をDBに保存

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid');
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
+});
+
+router.get('/add', (req, res, next) => {
+  const title = '新規ユーザー登録';
+  res.render('users/add', {
+    title: title
+  });
+});
+
+router.post('/add', (req, res, next) => {
+  const params = {
+    userId: uuid.v4(),
+    username: req.body.username.slice(0, 255),
+    email: req.body.email,
+    nickname: req.body.nickname,
+    description: req.body.description
+  };
+  User.create(params).then(() => {
+    res.redirect('/');
+  });;
 });
 
 module.exports = router;
